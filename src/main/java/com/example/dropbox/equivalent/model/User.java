01d84ae6167d39da1a30b890c43b201bc380f560
Samyak Singh <ssesamyak@gmail.com>
2023-11-18 09:55:26 +0530

    Initial commit

---
+++ src/main/java/com/example/dropbox/equivalent/model/User.java
@@ -0,0 +1,21 @@
+package com.example.dropbox.equivalent.model;
+
+public class User {
+}
+
+
+/*
+* User
+* UserID
+* Username
+* email
+*  */
+
+
+/*
+File
+fileid
+filename
+filesize
+filedata
+ */
