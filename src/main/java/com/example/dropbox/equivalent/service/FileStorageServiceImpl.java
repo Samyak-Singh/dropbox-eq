866c79398190586351957ffe2533549116a7dd5f
Samyak Singh <ssesamyak@gmail.com>
2023-11-18 11:29:27 +0530

    Add file storage in the database

--- src/main/java/com/example/dropbox/equivalent/service/FileStorageServiceImpl.java
+++ src/main/java/com/example/dropbox/equivalent/service/FileStorageServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.dropbox.equivalent.service;
 
 import com.example.dropbox.equivalent.model.File;
+import com.example.dropbox.equivalent.repository.FileRepository;
 import com.example.dropbox.equivalent.service.FileStorageService;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,11 +14,16 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.logging.Level;
 
 @Service
+@Log
 public class FileStorageServiceImpl implements FileStorageService {
 
     private final String uploadDir = "./uploads"; // Change this to your desired directory
+
+    @Autowired
+    FileRepository fileRepository;
 
     @Override
     public String storeFile(MultipartFile file, String fileName, String metadata) throws IOException {
@@ -32,6 +40,13 @@ public class FileStorageServiceImpl implements FileStorageService {
         fileModel.setCreatedAt(Timestamp.from(createdAt.toInstant()));
         fileModel.setFileType(fileType);
         fileModel.setSize(String.valueOf(size));
+        try {
+            fileRepository.save(fileModel);
+            log.log(Level.INFO, "File iinfo saved in the database  ID :{}  ",  fileModel.getFileId());
+        }
+        catch (Exception e) {
+
+        }
 
         Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 
